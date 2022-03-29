b3bbb0dea26377d7d2d6601830f41afb43f326cf
jaechik <jaexus@hotmail.com>
2022-03-29 23:02:41 +0800

    Not sure if correct way, need advice

--- trueFalse.go
+++ trueFalse.go
@@ -2,12 +2,22 @@ package main
 
 import "fmt"
 
+
+var input = 10
+
 func compare(value int) string {
 	//do not change this variable resultMessage, secretValue
 	resultMessge := ""
 	secretValue := 88
 
 	//Insert your code from here
+for input := ""; input < secretValue; input ++ {
+	fmt.Println("Too Low, try again next time!");
+} else if input := ""; input > secretValue; input ++ {
+	fmt.Println("Too High, try again next time!");
+} else if input == secretValue {
+	fmt.Println("Well Done! Your guess is correct");
+}
 
 	//do not remove this line
 	return resultMessge
